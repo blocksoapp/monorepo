7c93030674788b938a104e9ced20cb6967311048
nullbitx8 <nullbitx8@protonmail.com>
2022-09-25 10:27:45 +0100

    update wording

--- frontend/src/pages/Explore/InfoSection.js
+++ frontend/src/pages/Explore/InfoSection.js
@@ -7,11 +7,12 @@ function InfoSection() {
         <Container fluid className="p-2 p-sm-5">
             <div className="border info-container border p-4 d-flex justify-content-center align-item-center">
                 <div className='w-75 p-1 p-sm-3'>
-                    <h1>A Social Platform Curated for <span className="primary-color">WEB3</span></h1>
-                    <p className="">Blockso is Twitter for blockchain users. 
-                    Follow your favorite wallets closely, without endlessly navigating through etherscan. 
-                    With Blockso, you have the ability to keep up to date with web3 trends at the tip of your fingers. 
-                    Connect your wallet to get started.</p>
+                    <h1>A Social Platform for <span className="primary-color">WEB3</span></h1>
+                    <p className="fs-4 lh-sm">
+                        Blockso is a new way to keep up with your crypto frens!
+                        <br/>
+                        Follow your favorite personalities, share your latest, and collect that sweet internet karma :)
+                    </p>
                     <div className="mt-2">
                         <Button className="btn-lg custom-btn" href="https://github.com/blocksoapp/monorepo" target="_blank">Learn More</Button>
                     </div>
