a41c8859cdbba5912f358de5e75895d4a4599f50
Alex <iambethharmon@protonmail.com>
2022-12-22 21:26:16 -0500

    added toast notifying users after 8 hours of being signed in

---
+++ frontend/src/components/authentication/SessionExpiredToast.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { Toast, ToastContainer } from "react-bootstrap";
+import { ConnectKitButton } from "connectkit";
+import { UserContext } from "../../contexts/UserContext";
+
+function SessionExpiredToast(props) {
+  const { isAuthenticated } = useContext(UserContext);
+  return (
+    <>
+      {props.isToggle ? (
+        <ToastContainer position="top-end" className="p-3">
+          <Toast
+            show={props.isToggle}
+            onClose={() => props.setIsToggle(false)}
+            autohide={isAuthenticated}
+            delay={0}
+          >
+            <Toast.Header>
+              <strong className="me-auto">Session Expired</strong>
+            </Toast.Header>
+            <Toast.Body>
+              Your session has timed out. Please sign in again.{" "}
+              <ConnectKitButton />
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+export default SessionExpiredToast;
--- frontend/src/components/ui/Navbar.js
+++ frontend/src/components/ui/Navbar.js
@@ -1,58 +1,75 @@
-import { useState, useContext, useRef } from 'react';
-import { Button, Container, Col, Form, Nav, Navbar, NavDropdown, NavItem } from 'react-bootstrap'
-import { NavLink, Link, useNavigate } from 'react-router-dom'
-import { useAccount } from 'wagmi';
-import SignInButton from '../authentication/SignInButton';
-import NotificationsDropdown from '../notifications/NotificationsDropdown';
-import { UserContext } from '../../contexts/UserContext'
+import { useState, useContext } from "react";
+import { Button, Container, Col, Form, Nav, Navbar } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useAccount } from "wagmi";
+import SignInButton from "../authentication/SignInButton";
+import NotificationsDropdown from "../notifications/NotificationsDropdown";
+import { UserContext } from "../../contexts/UserContext";
 
 function NavbarComponent() {
-    const { user } = useContext(UserContext)
-    const navigate = useNavigate();
-    const account = useAccount();
-    const [searchVal, setSearchVal] = useState("");
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  const [searchVal, setSearchVal] = useState("");
 
-    const handleSearch = () => {
-        const route = `${searchVal}/profile`;
-        navigate(route);
-        setSearchVal("");  // clear input
+  const handleSearch = () => {
+    const route = `${searchVal}/profile`;
+    navigate(route);
+    setSearchVal(""); // clear input
+  };
+
+  const onKeyPress = (event) => {
+    // search when user presses enter
+    if (event.key === "Enter") {
+      handleSearch();
     }
-
-    const onKeyPress = (event) => {
-        // search when user presses enter
-        if (event.key === "Enter") {
-            handleSearch();
-        }
-    }
-
+  };
 
   return (
     <Navbar bg="light" expand="lg" className="mb-5">
       <Container>
-        <Navbar.Brand as={Link} to="/">Blockso</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Blockso
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="w-100">
-            {user !== null && <Nav.Link as={Link} to="/home">Home</Nav.Link>}
-            <Nav.Link as={Link} to="/explore">Explore</Nav.Link>
-            {user !== null && <Nav.Link as={Link} to="/edit-profile">Edit Profile</Nav.Link>}
-            {user !== null && <Nav.Link as={Link} to={`${user["address"]}/profile`}>My Profile</Nav.Link>}
-            <Col className="col-auto">
-            </Col>
+            {user !== null && (
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+            )}
+            <Nav.Link as={Link} to="/explore">
+              Explore
+            </Nav.Link>
+            {user !== null && (
+              <Nav.Link as={Link} to="/edit-profile">
+                Edit Profile
+              </Nav.Link>
+            )}
+            {user !== null && (
+              <Nav.Link as={Link} to={`${user["address"]}/profile`}>
+                My Profile
+              </Nav.Link>
+            )}
+            <Col className="col-auto"></Col>
             <Col xs={10} lg={5}>
-                <Form.Control
-                  type="search"
-                  placeholder="Search ENS or address"
-                  className="me-2"
-                  aria-label="Search"
-                  value={searchVal}
-                  onChange={event => {setSearchVal(event.target.value)}}
-                  onKeyPress={onKeyPress}
-                />
+              <Form.Control
+                type="search"
+                placeholder="Search ENS or address"
+                className="me-2"
+                aria-label="Search"
+                value={searchVal}
+                onChange={(event) => {
+                  setSearchVal(event.target.value);
+                }}
+                onKeyPress={onKeyPress}
+              />
             </Col>
             &nbsp;
             <Col xs={2}>
-                <Button variant="outline-dark" onClick={handleSearch}>Search</Button>
+              <Button variant="outline-dark" onClick={handleSearch}>
+                Search
+              </Button>
             </Col>
           </Nav>
         </Navbar.Collapse>
