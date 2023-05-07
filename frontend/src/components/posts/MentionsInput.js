dbd1b9fdaa8ff3a06454c4759f1342a226bed5bc
Alex <iambethharmon@protonmail.com>
2023-05-07 17:02:26 -0400

    change textarea height to 100px

--- frontend/src/components/posts/MentionsInput.js
+++ frontend/src/components/posts/MentionsInput.js
@@ -1,78 +1,66 @@
 /*
  * Wraps the react-mentions MentionsInput for Blockso's purposes.
  */
-import {
-    MentionsInput as ReactMentionsInput,
-    Mention
-} from "react-mentions";
-import { abbrAddress } from '../../utils'
-import { apiGetSuggestedUsers } from '../../api';
+import { MentionsInput as ReactMentionsInput, Mention } from "react-mentions";
+import { abbrAddress } from "../../utils";
+import { apiGetSuggestedUsers } from "../../api";
 import "./mentions-custom.css";
 
+function MentionsInput({ placeholder, text, setText, setTaggedUsers }) {
+  /*
+   * Requests a list of users that start with the given query.
+   * Calls callback with the results of the response.
+   */
+  const fetchSuggestedUsers = async (query, callback) => {
+    const resp = await apiGetSuggestedUsers(query);
+    if (resp.ok) {
+      const results = (await resp.json()).results;
+      var formatted = results.map((user) => ({
+        display: `@0x${abbrAddress(user.address)}`,
+        id: user.address,
+      }));
 
-function MentionsInput({placeholder, text, setText, setTaggedUsers}) {
+      // include @everyone when the user starts to type
+      if ("everyone".startsWith(query)) {
+        formatted.unshift({ display: "@everyone", id: "everyone" });
+      }
+      return callback(formatted);
+    }
+  };
 
-    /*
-     * Requests a list of users that start with the given query.
-     * Calls callback with the results of the response.
-     */
-    const fetchSuggestedUsers = async (query, callback) => {
-        const resp = await apiGetSuggestedUsers(query);
-        if (resp.ok) {
-            const results = (await resp.json()).results;
-            var formatted = results.map(
-                user => ({
-                    display: `@0x${abbrAddress(user.address)}`,
-                    id: user.address
-                })
-            );
+  /*
+   * Parses user addresses from the input's text.
+   * Returns an array of user addresses.
+   */
+  const parseTaggedUsers = (inputText) => {
+    // look for mentions pattern and capture the address
+    // mentions pattern: @[__display__](__0xaddress__)
+    const regexp = /@\[@[\w\.]*\]\((0x\w{40}|everyone)\)/gim;
+    const matches = [...inputText.matchAll(regexp)];
 
-            // include @everyone when the user starts to type
-            if ("everyone".startsWith(query)) {
-                formatted.unshift({display: '@everyone', id: 'everyone'});
-            }
-            return callback(formatted);
-        }
+    // create array of mentioned user addresses
+    var addresses = [];
+    for (const match of matches) {
+      addresses.push(match[1]);
     }
 
-    /*
-     * Parses user addresses from the input's text.
-     * Returns an array of user addresses.
-     */
-    const parseTaggedUsers = (inputText) => {
-        // look for mentions pattern and capture the address
-        // mentions pattern: @[__display__](__0xaddress__)
-        const regexp = /@\[@[\w\.]*\]\((0x\w{40}|everyone)\)/gmi;
-        const matches = [...inputText.matchAll(regexp)];
+    setTaggedUsers(addresses);
+  };
 
-        // create array of mentioned user addresses
-        var addresses = [];
-        for (const match of matches) {
-            addresses.push(match[1]);
-        }
-
-        setTaggedUsers(addresses);
-    }
-
-
-    return (
-        <ReactMentionsInput
-            className="form-control"
-            value={text}
-            onChange={(event) => {
-                setText(event.target.value);
-                parseTaggedUsers(event.target.value);
-            }}
-            placeholder={placeholder}
-        >
-            <Mention
-                trigger="@"
-                data={fetchSuggestedUsers}
-                appendSpaceOnAdd={true}
-            />
-        </ReactMentionsInput>
-    )
+  return (
+    <ReactMentionsInput
+      className="form-control"
+      style={{ height: "100px" }}
+      value={text}
+      onChange={(event) => {
+        setText(event.target.value);
+        parseTaggedUsers(event.target.value);
+      }}
+      placeholder={placeholder}
+    >
+      <Mention trigger="@" data={fetchSuggestedUsers} appendSpaceOnAdd={true} />
+    </ReactMentionsInput>
+  );
 }
-
 
 export default MentionsInput;
