f46468c83623e78bd5d8b8021a3bd595e30237e0
Alex <iambethharmon@protonmail.com>
2023-05-08 12:04:04 -0400

    adjust createfeed button padding

--- frontend/src/components/feed/write/CreateFeedButton.js
+++ frontend/src/components/feed/write/CreateFeedButton.js
@@ -3,33 +3,28 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import { UserContext } from "../../../contexts/UserContext";
 import CreateFeedModal from "./CreateFeedModal";
 
-
 function CreateFeedButton() {
-    // hooks
-    const { user } = useContext(UserContext);
+  // hooks
+  const { user } = useContext(UserContext);
 
-    // state
-    // show or hide modal when the button is clicked
-    const [show, setShow] = useState(false);
+  // state
+  // show or hide modal when the button is clicked
+  const [show, setShow] = useState(false);
 
-
-    return (
-        user !== null && (
-            <Container>
-                <CreateFeedModal show={show} setShow={setShow} />
-                <Row>
-                    <Col className="text-end">
-                        <Button
-                            variant="success"
-                            onClick={() => setShow(true)}
-                        >
-                            Create Feed
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        )
+  return (
+    user !== null && (
+      <Container>
+        <CreateFeedModal show={show} setShow={setShow} />
+        <Row>
+          <Col className="text-end" style={{ padding: "0 12px" }}>
+            <Button variant="success" onClick={() => setShow(true)}>
+              Create Feed
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     )
+  );
 }
 
 export default CreateFeedButton;
