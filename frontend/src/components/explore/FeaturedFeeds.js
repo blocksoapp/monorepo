848e4013c451b2f11731a98d3d5e80e8b42d4387
Alex <iambethharmon@protonmail.com>
2023-05-08 13:07:14 -0400

    changed col grid size on small screen

--- frontend/src/components/explore/FeaturedFeeds.js
+++ frontend/src/components/explore/FeaturedFeeds.js
@@ -1,44 +1,38 @@
-import React, { useContext, } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, Col, Container, Row } from 'react-bootstrap';
-import { UserContext } from '../../contexts/UserContext.js';
-import FeedThumbnail from '../feed/read/FeedThumbnail.js';
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { UserContext } from "../../contexts/UserContext.js";
+import FeedThumbnail from "../feed/read/FeedThumbnail.js";
 
+function FeaturedFeeds({ feeds }) {
+  // hooks
+  const { user, setUser, isAuthenticated } = useContext(UserContext);
+  const navigate = useNavigate();
 
-function FeaturedFeeds({feeds}) {
-    // hooks
-    const { user, setUser, isAuthenticated } = useContext(UserContext)
-    const navigate = useNavigate();
-    
-
-    // render
-    return (
-        <Container>
-            <Row>
-                {feeds.map(feed => (
-                    <Col xs={6}>
-                        <FeedThumbnail
-                            key={feed.id}
-                            data={feed}
-                        />
-                    </Col>
-                ))}
-            </Row>
-            <Row className="justify-content-center">
-                <Col className="col-auto">
-                    <Button
-                        className="my-4"
-                        variant="outline-primary"
-                        size="lg"
-                        onClick={() => navigate('/feeds')}
-                    >
-                        View All
-                    </Button>
-                </Col>
-            </Row>
-        </Container>
-    );
+  // render
+  return (
+    <Container>
+      <Row>
+        {feeds.map((feed) => (
+          <Col xs={12} md={6}>
+            <FeedThumbnail key={feed.id} data={feed} />
+          </Col>
+        ))}
+      </Row>
+      <Row className="justify-content-center">
+        <Col className="col-auto">
+          <Button
+            className="my-4"
+            variant="outline-primary"
+            size="lg"
+            onClick={() => navigate("/feeds")}
+          >
+            View All
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
 
 export default FeaturedFeeds;
