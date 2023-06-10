5d3fea38c1d0b0be3cea177e6318ce7f4dfe0516
nullbitx8 <nullbitx8@protonmail.com>
2023-06-11 00:00:26 +0100

    Frontend: fix metamask/walletconnect issue on mobile

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,12 @@ import '@rainbow-me/rainbowkit/styles.css';
 import "react-image-lightbox/style.css";
 import App from "./App";
 import Middleware from "./middleware/Middleware";
+import { Buffer } from "buffer";
+
+
+// workaround for
+// https://github.com/WalletConnect/walletconnect-monorepo/issues/748
+window.Buffer = window.Buffer || Buffer;
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
